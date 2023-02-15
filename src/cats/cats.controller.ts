9f90ac37cd0e7e40baa259815210d5c4374c3c1e
jzzyy <passby158@gmail.com>
2023-02-15 13:23:15 +0900

    미들웨어 구축

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCats() {
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCate() {
+    return 'one cat';
+  }
+
+  @Post(':id')
+  createCats() {
+    return 'create cat';
+  }
+
+  @Delete(':id')
+  deleteCate() {
+    return 'delete cat';
+  }
+
+  @Put(':id')
+  updateCats() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCats() {
+    return 'update';
+  }
+}
